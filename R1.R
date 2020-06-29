722923e7aae9abb425ddb94f02f04fe4a101aaee
RITHIKKATHAIT123 <66545962+RITHIKKATHAIT123@users.noreply.github.com>
2020-06-29 21:39:55 +0530

    Add files via upload

---
+++ R1.R
@@ -0,0 +1,13 @@
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
+}
+Tips for s
